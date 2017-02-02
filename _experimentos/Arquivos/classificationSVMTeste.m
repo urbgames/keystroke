0cfed63b01c912d708ce5fd70ad4d94a8538f2bb
Urbgames <urbgames@gmail.com>
2017-02-02 16:08:57 -0300

    initial

---
+++ _experimentos/Arquivos/classificationSVMTeste.m
@@ -0,0 +1,35 @@
+function [validationAccuracy] = classificationSVMTeste()
+tic
+load baseMobile
+
+data = baseMobile;
+
+% CREATE PREDICTORS
+predictors = data;
+
+% DEFINE CLASS INDEX
+classIndex = 69;
+
+% REMOVE COLUMN CLASSINDEX
+predictors(:,classIndex) = [];
+
+% GET LABELS TABLE
+predictorNames = predictors.Properties.VariableNames;
+
+predictors = table2array(varfun(@double, predictors));
+response = data.user_id;
+
+% DEFINE CLASS NAMES
+classNames = unique(response)';
+
+% Train a classifier
+template = templateSVM();
+trainedClassifier = fitcecoc(predictors, response, 'Learners', template, 'Coding', 'onevsone', 'PredictorNames', predictorNames, 'ResponseName', 'user_id', 'ClassNames', classNames);
+
+% Perform cross-validation
+partitionedModel = crossval(trainedClassifier, 'KFold', 10);
+
+% Compute validation accuracy
+validationAccuracy = kfoldLoss(partitionedModel, 'LossFun', 'ClassifError');
+
+toc
