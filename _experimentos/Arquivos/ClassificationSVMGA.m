e7b663c7a978eb4580747feb04bc6c6cb988196d
Urbgames <urbgames@gmail.com>
2017-02-09 20:27:15 -0300

    backup

---
+++ _experimentos/Arquivos/ClassificationSVMGA.m
@@ -0,0 +1,45 @@
+function [validationAccuracy] = ClassificationSVMGA(individual)
+%tic
+load baseMobile
+
+data = baseMobile;
+
+% CREATE PREDICTORS
+predictors = data;
+
+% DEFINE CLASS INDEX
+classIndex = 69;
+
+% REMOVE COLUMN CLASSINDEX
+predictors(:,classIndex) = [];
+
+elementsRemove = [];
+
+for i = 1:length(individual)
+    if individual(:,i) == 0
+        elementsRemove = [elementsRemove i];
+    end
+end
+
+predictors(:,elementsRemove) = [];
+
+% GET LABELS TABLE
+predictorNames = predictors.Properties.VariableNames;
+
+predictors = table2array(varfun(@double, predictors));
+response = data.user_id;
+
+% DEFINE CLASS NAMES
+classNames = unique(response)';
+
+% Train a classifier
+template = templateSVM('KernelFunction', 'linear', 'PolynomialOrder', [], 'KernelScale', 'auto', 'BoxConstraint', 1, 'Standardize', 1);
+trainedClassifier = fitcecoc(predictors, response, 'Learners', template, 'Coding', 'onevsone', 'PredictorNames', predictorNames, 'ResponseName', 'user_id', 'ClassNames', classNames);
+
+% Perform cross-validation
+partitionedModel = crossval(trainedClassifier, 'KFold', 10);
+
+% Compute validation accuracy
+validationAccuracy = kfoldLoss(partitionedModel, 'LossFun', 'ClassifError');
+
+%toc
