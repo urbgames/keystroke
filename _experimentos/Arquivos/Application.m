22708efbe733413d076318668dfc077e78370d05
Urbgames <urbgames@gmail.com>
2017-02-24 22:42:28 -0300

    Inicio dos experimentos

--- _experimentos/Arquivos/Application.m
+++ _experimentos/Arquivos/Application.m
@@ -5,7 +5,7 @@ tic
 nvars = 68;
 %EliteCount_Data = 5; %Quantidade de pais para reprodução
 MaxGenerations_Data = 200; %Quantidade de gerações - Variar ao longo dos experimentos
-PopulationSize_Data = 50; % Tamanho da população
+PopulationSize_Data = 150; % Tamanho da população
 FunctionTolerance_Data = 0;
 
 [x,fval,exitflag,output,population,score] = GA(nvars,PopulationSize_Data,MaxGenerations_Data,FunctionTolerance_Data);
